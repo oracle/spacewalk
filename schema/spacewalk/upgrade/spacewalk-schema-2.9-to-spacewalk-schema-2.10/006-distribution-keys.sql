cb63d32d1a8c359e912a319a8c9d70fbcdf57531
Laurence Rochfort <laurence.rochfort@oracle.com>
2020-10-08 11:27:23 +0100

    2.10 schema upgrade fails due to existing distro keys.
    Distro keys are sometimes already present when upgrading to 2.10
    causing a unique constraint violation that causes the script to stop.
    
    Update upgrade script to only insert if keys not already present.
    
    RHBZ: 1815180
    Orabug: 31957738
    
    Signed-off-by: Laurence Rochfort <laurence.rochfort@oracle.com>
    Reviewed-by: Alex Burmashev <alexander.burmashev@oracle.com>

--- schema/spacewalk/upgrade/spacewalk-schema-2.9-to-spacewalk-schema-2.10/006-distribution-keys.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-2.9-to-spacewalk-schema-2.10/006-distribution-keys.sql
@@ -1,27 +1,133 @@
 -- Fedora 30
-insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
-(sequence_nextval('rhn_pkey_id_seq'), 'CFC659B9', lookup_package_key_type('gpg'), lookup_package_provider('Fedora'));
--- Fedora 31
-insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
-(sequence_nextval('rhn_pkey_id_seq'), '3C3359C4', lookup_package_key_type('gpg'), lookup_package_provider('Fedora'));
+INSERT INTO rhnpackagekey
+            (id,
+             key_id,
+             key_type_id,
+             provider_id)
+SELECT sequence_nextval('rhn_pkey_id_seq'),
+       'CFC659B9',
+       lookup_package_key_type('gpg'),
+       lookup_package_provider('Fedora')
+ FROM dual
+WHERE NOT EXISTS (
+     SELECT *
+       FROM rhnpackagekey pk
+      WHERE pk.key_id = 'CFC659B9'
+);
 
+-- Fedora 31
+INSERT INTO rhnpackagekey
+            (id,
+             key_id,
+             key_type_id,
+             provider_id)
+SELECT sequence_nextval('rhn_pkey_id_seq'),
+       '3C3359C4',
+        lookup_package_key_type('gpg'),
+        lookup_package_provider('Fedora')
+ FROM dual
+WHERE NOT EXISTS (
+      SELECT *
+        FROM rhnpackagekey pk
+       WHERE pk.key_id = '3C3359C4'
+);
 
 -- CentOS 8
-insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
-(sequence_nextval('rhn_pkey_id_seq'), '05b555b38483c65d', lookup_package_key_type('gpg'), lookup_package_provider('CentOS'));
+INSERT INTO rhnpackagekey
+            (id,
+             key_id,
+             key_type_id,
+             provider_id)
+SELECT sequence_nextval('rhn_pkey_id_seq'),
+       '05b555b38483c65d',
+       lookup_package_key_type('gpg'),
+       lookup_package_provider('CentOS')
+ FROM dual
+WHERE NOT EXISTS (
+      SELECT *
+        FROM rhnpackagekey pk
+       WHERE pk.key_id =  '05b555b38483c65d'
+);
 
 -- Spacewalk 2.10
-insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
-(sequence_nextval('rhn_pkey_id_seq'), '3ae9b50430912c76', lookup_package_key_type('gpg'), lookup_package_provider('Spacewalk'));
-insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
-(sequence_nextval('rhn_pkey_id_seq'), '770ce53ebc2e6843', lookup_package_key_type('gpg'), lookup_package_provider('Spacewalk'));
+INSERT INTO rhnpackagekey
+            (id,
+             key_id,
+             key_type_id,
+             provider_id)
+SELECT sequence_nextval('rhn_pkey_id_seq'),
+       '3ae9b50430912c76',
+       lookup_package_key_type('gpg'),
+       lookup_package_provider('Spacewalk')
+ FROM dual
+WHERE NOT EXISTS (
+      SELECT *
+        FROM rhnpackagekey pk
+       WHERE pk.key_id = '3ae9b50430912c76'
+);
+
+INSERT INTO rhnpackagekey
+            (id,
+             key_id,
+             key_type_id,
+             provider_id)
+SELECT sequence_nextval('rhn_pkey_id_seq'),
+       '770ce53ebc2e6843',
+       lookup_package_key_type('gpg'),
+       lookup_package_provider('Spacewalk')
+ FROM dual
+WHERE NOT EXISTS (
+      SELECT *
+        FROM rhnpackagekey pk
+       WHERE pk.key_id = '770ce53ebc2e6843'
+);
+
 -- Spacewalk nightly
-insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
-(sequence_nextval('rhn_pkey_id_seq'), 'e481344adba67ea3', lookup_package_key_type('gpg'), lookup_package_provider('Spacewalk'));
-insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
-(sequence_nextval('rhn_pkey_id_seq'), 'd4b984391b9881e5', lookup_package_key_type('gpg'), lookup_package_provider('Spacewalk'));
+INSERT INTO rhnpackagekey
+            (id,
+             key_id,
+             key_type_id,
+             provider_id)
+SELECT sequence_nextval('rhn_pkey_id_seq'),
+       'e481344adba67ea3',
+       lookup_package_key_type('gpg'),
+       lookup_package_provider('Spacewalk')
+ FROM dual
+WHERE NOT EXISTS (
+      SELECT *
+        FROM rhnpackagekey pk
+       WHERE pk.key_id = 'e481344adba67ea3'
+);
+
+INSERT INTO rhnpackagekey
+            (id,
+             key_id,
+             key_type_id,
+             provider_id)
+SELECT sequence_nextval('rhn_pkey_id_seq'),
+       'd4b984391b9881e5',
+       lookup_package_key_type('gpg'),
+       lookup_package_provider('Spacewalk')
+ FROM dual
+WHERE NOT EXISTS (
+      SELECT *
+        FROM rhnpackagekey pk
+       WHERE pk.key_id = 'd4b984391b9881e5'
+);
 
 -- EPEL 8
-insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
-(sequence_nextval('rhn_pkey_id_seq'), '21ea45ab2f86d6a1', lookup_package_key_type('gpg'), lookup_package_provider('EPEL'));
- 
+INSERT INTO rhnpackagekey
+            (id,
+             key_id,
+             key_type_id,
+             provider_id)
+SELECT sequence_nextval('rhn_pkey_id_seq'),
+       '21ea45ab2f86d6a1',
+       lookup_package_key_type('gpg'),
+       lookup_package_provider('EPEL')
+ FROM dual
+WHERE NOT EXISTS (
+      SELECT *
+        FROM rhnpackagekey pk
+       WHERE pk.key_id = '21ea45ab2f86d6a1'
+);
